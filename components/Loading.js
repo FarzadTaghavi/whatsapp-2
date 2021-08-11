dd27867852b8eae018035786bb9bf30bf8ca4bbe
Farzad <fmtaghavi@gmail.com>
2021-08-11 15:27:28 +0200

    img bug fix for deployment in Loading component

--- components/Loading.js
+++ components/Loading.js
@@ -1,13 +1,16 @@
 import { Circle } from "better-react-spinkit";
+import Image from "next/image";
+import ws from "../images/whatsapp.png";
 
 function Loading() {
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
-        <img
-          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+        <Image
+          src={ws}
           alt="whatsapp"
           height={200}
+          width={200}
           style={{ marginBottom: 10 }}
         />
         <Circle color="#3CBC28" size={60} />
